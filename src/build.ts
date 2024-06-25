b5e4b98f440dd85e90b16b10afdced4fc38d979e
takokun <mukoudakota8888@gmail.com>
2024-06-26 03:42:30 +0900

    @tako-dayo8 fix: @minecraft/serverを再インストール

--- src/build.ts
+++ src/build.ts
@@ -11,7 +11,7 @@ if (!fs.existsSync('build')) {
     fs.mkdirSync('build');
 }
 
-execSync('tsc'); // TypeScriptをコンパイル
+execSync('tsc --diagnostics'); // TypeScriptをコンパイル
 
 // srcフォルダ内のファイルを./behavior_packs/**/scripts 以外をbuildフォルダにコピー
 const files = globSync(['./behavior_packs/**/*', './resource_packs/**/*'], { ignore: ['./behavior_packs/**/scripts/**/*', './behavior_packs/**/scripts/'] }).map((file) => file.replace(/\\/g, '/'));
